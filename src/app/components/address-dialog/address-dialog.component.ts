16200e9936238016b01789c9ce70cad87209b526
João Fernandes <joao.fernandes450@gmail.com>
2021-02-08 18:19:42 +0000

    OpenLayers Map should now display an icon marker

--- src/app/components/address-dialog/address-dialog.component.ts
+++ src/app/components/address-dialog/address-dialog.component.ts
@@ -6,6 +6,12 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj';
+import Point from 'ol/geom/Point';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import { Feature } from 'ol';
+import { Icon, Style } from 'ol/style';
+import TileJSON from 'ol/source/TileJSON';
 
 @Component({
   selector: 'app-address-dialog',
@@ -23,17 +29,82 @@ export class AddressDialogComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     const convertedWebMercator = fromLonLat([this.data.longitude, this.data.latitude]);
 
+    const address = new Feature({
+      geometry: new Point(convertedWebMercator),
+      name: 'teste',
+    })
+    address.setStyle(
+      new Style({
+        image: new Icon({
+          crossOrigin: 'anonymous',
+          // For Internet Explorer 11
+          src: 'assets/marker3.png',
+          scale: 0.09
+        })
+      })
+    )
+    var vectorSource = new VectorSource({
+      features: [address],
+    });
+    var vectorLayer = new VectorLayer({
+      source: vectorSource,
+    });
+    var rasterLayer = new TileLayer({
+      source: new OSM(),
+    });
+
     this.map = new Map({
+      layers: [rasterLayer, vectorLayer],
+      target: 'map',
       view: new View({
         center: convertedWebMercator,
         zoom: 15
       }),
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        })
-      ],
-      target: 'map'
     });
+
+    // const markerSource = new VectorSource();
+    // const markerStyle = new Style({
+    //   image: new Icon({
+    //     anchor: [0.5, 46],
+
+    //     src: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',
+    //   }),
+    // });
+
+    // this.map = new Map({
+    //   view: new View({
+    //     center: convertedWebMercator,
+    //     zoom: 15
+    //   }),
+    //   layers: [
+    //     new TileLayer({
+    //       source: new OSM()
+    //     }),
+    //     new VectorLayer({
+    //       source: markerSource,
+    //     })
+    //   ],
+    //   target: 'map'
+    // });
+
+
+    // const address = new Feature({
+    //   geometry: new Point(convertedWebMercator),
+    //   name: 'teste',
+    // })
+    // address.setStyle(
+    //   new Style({
+    //     image: new Icon({
+    //       color: '#BADA55',
+    //       crossOrigin: 'anonymous',
+    //       // For Internet Explorer 11
+    //       imgSize: [20, 20],
+    //       src: 'assets/1.jpg',
+    //     })
+    //   })
+    // )
+
+    // markerSource.addFeature(iconFeature);
+
   }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -10,6 +10,9 @@ import { DataService } from 'src/app/services/data/data.service';
 
 export class CustomValidators {
 
+  /**
+   * Email pattern validator
+   */
   static email(): ValidatorFn {
     return Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   }
